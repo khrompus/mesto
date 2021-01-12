6ed1ecacf589049fd0755b5747098bab8f11bb15
Vlad Rakhmaev <khrompus@yandex.ru>
2021-01-12 16:52:59 +0500

    hotfix

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,27 @@
 let openPopupBtn = document.querySelector('.profile__button-edit');
-
-let popup = document.querySelector('.overlay');
-
+let popup = document.querySelector('.popup');
 let popupExit = popup.querySelector('.popup__exit-btn');
-
 let title = document.querySelector('.profile__title');
-
 let subtitle = document.querySelector('.profile__subtitle');
-
 let formElement = document.querySelector('.popup__form');
-
-let popupName = formElement.querySelector('.popup__name');
-
-let popupNameSubtitle = formElement.querySelector('.popup__name-info');
-
-let submit = document.querySelector('.popup__submit-btn');
-
-
-
-let openPopup = () => {
-    popup.classList.toggle('overlay__active')
+let popupName = formElement.querySelector('.popup__name_firstName');
+let popupNameSubtitle = formElement.querySelector('.popup__name_aboutMe');
+let togglePopup = () => {
+    popup.classList.toggle('popup__active')
+    popupName.value = title.textContent
+    popupNameSubtitle.value = subtitle.textContent
 }
-popupName.value = title.textContent
-popupNameSubtitle.value = subtitle.textContent
+openPopupBtn.addEventListener('click', togglePopup);
+popupExit.addEventListener('click', togglePopup);
 
-
-openPopupBtn.addEventListener('click', openPopup);
-popupExit.addEventListener('click', openPopup);
-
-function rename(evt) {
-evt.preventDefault()
-
- title.textContent = popupName.value;
- subtitle.textContent = popupNameSubtitle.value;
- popup.classList.toggle('overlay__active')
+function handleFormSubmit(evt) {
+    evt.preventDefault()
+    title.textContent = popupName.value;
+    subtitle.textContent = popupNameSubtitle.value;
+    togglePopup()
 }
-formElement.addEventListener('submit', rename);
+
+formElement.addEventListener('submit', handleFormSubmit);
 
 
 
