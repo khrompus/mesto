d5363fa2d534026b265f791dbada5fb73cb63a6d
Vlad Rakhmaev <khrompus@yandex.ru>
2021-01-19 01:51:09 +0500

    feat: Интерактивные детали

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,49 @@ let subtitle = document.querySelector('.profile__subtitle');
 let formElement = document.querySelector('.popup__form');
 let popupName = formElement.querySelector('.popup__input_type_first-name');
 let popupNameSubtitle = formElement.querySelector('.popup__input_type_about-me');
+let templateGrid = document.querySelector('.grid-template').content;
+let gridContainer = document.querySelector('.grid');
+const gridArray = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+gridArray.forEach(function (element) {
+    const gridElement = templateGrid.cloneNode(true);
+    gridElement.querySelector('.grid__image').src = element.link;
+    gridElement.querySelector('.grid__text').textContent = element.name;
+    // like active
+    gridElement.querySelector('.grid__like').addEventListener('click', function (evt) {
+        evt.target.classList.toggle('grid__like_active');
+    })
+    //remove grid items
+    let removeItem = gridElement.querySelector('.grid__items');
+    gridElement.querySelector('.grid__delete-btn').addEventListener('click', function (event) {
+        event.target = removeItem.remove();
+    })
+    gridContainer.append(gridElement);
+})
 let activePopup = () => {
     popup.classList.add('popup_active')
     popupName.value = title.textContent
@@ -23,5 +66,4 @@ function handleFormSubmit(evt) {
     subtitle.textContent = popupNameSubtitle.value;
     exitPopup();
 }
-
 formElement.addEventListener('submit', handleFormSubmit);
