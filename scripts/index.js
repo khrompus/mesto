a3b1a4485b5b281c7c65520b6317082548d289f2
Vlad Rakhmaev <khrompus@yandex.ru>
2021-01-21 18:48:37 +0500

    feat:image popup

--- scripts/index.js
+++ scripts/index.js
@@ -8,10 +8,14 @@ let popupName = formElement.querySelector('.popup__input_type_first-name');
 let popupNameSubtitle = formElement.querySelector('.popup__input_type_about-me');
 let templateGrid = document.querySelector('.grid-template').content;
 let gridContainer = document.querySelector('.grid');
-let popupGrid = document.querySelector('.popup-grid');
+let popupGrid = document.querySelector('#popupAddCard');
 let popupGridImage = document.querySelector('.popup__input_type_image-name');
 let popupGridLink = document.querySelector('.popup__input_type_link');
+let popupOpenImage = document.querySelector('.grid__image-active');
+let popupImageExit = document.querySelector('.popup__image-exit-btn');
+let popupImage = document.querySelector('#popupImage');
 let openGridPopupBtn = document.querySelector('.profile__button-add');
+let exitGridPopup = document.querySelector('.popup__grid-exit-btn');
 const gridArray = [
     {
         name: 'Архыз',
@@ -42,17 +46,21 @@ gridArray.forEach(function (element) {
     const gridElement = templateGrid.cloneNode(true);
     gridElement.querySelector('.grid__image').src = element.link;
     gridElement.querySelector('.grid__text').textContent = element.name;
+    gridElement.querySelector('.grid__image').alt = element.name
     // like active
-    gridElement.querySelector('.grid__like').addEventListener('click', function (evt) {
-        evt.target.classList.toggle('grid__like_active');
-    })
+    gridElement.querySelector('.grid__like').addEventListener('click', likeActive)
     //remove grid items
-    let removeItem = gridElement.querySelector('.grid__items');
-    gridElement.querySelector('.grid__delete-btn').addEventListener('click', function (event) {
-        event.target = removeItem.remove();
-    })
+    gridElement.querySelector('.grid__delete-btn').addEventListener('click', deleteCard)
     gridContainer.append(gridElement);
 })
+
+function deleteCard(evt) {
+    evt.target.closest('.grid__items').remove();
+}
+function likeActive(evt) {
+    evt.target.classList.toggle('grid__like_active');
+}
+
 let activePopup = () => {
     popup.classList.add('popup_active')
     popupName.value = title.textContent
@@ -70,13 +78,18 @@ function handleFormSubmit(evt) {
     subtitle.textContent = popupNameSubtitle.value;
     exitPopup();
 }
+
 formElement.addEventListener('submit', handleFormSubmit);
 openGridPopupBtn.addEventListener('click', function () {
-popupGrid.classList.toggle('popup_active');
+    popupGrid.classList.add('popup_active');
 })
-popupExit.addEventListener('click', function () {
-popupGrid.classList.remove('popup_active');
+exitGridPopup.addEventListener('click', function () {
+    popupGrid.classList.remove('popup_active');
 })
-let popupGridArr = []
 
-
+popupImageExit.addEventListener('click', function () {
+    popupImage.classList.remove('popup_active');
+})
+popupOpenImage.addEventListener('click', function () {
+    popupImage.classList.add('popup_active');
+})
